9e44a96c9f5946e72e905111b08ae95d7644a002
Geovane Girard <g.ng2009@hotmail.com>
2025-02-10 19:18:51 -0300

    first commit of the project Mario Kart

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const player1 =  {
+    nome: "Mario",
+    velocidade: 4,
+    manobrabilidade: 3,
+    poder: 3,
+    pontos: 0,
+};
+
+const player2 =  {
+    nome: "Luigi",
+    velocidade: 3,
+    manobrabilidade: 4,
+    poder: 4,
+    pontos: 0,
+};
