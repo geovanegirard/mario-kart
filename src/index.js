036ffa921dac1f9628fd464c75887d26c5c2a601
Geovane Girard <g.ng2009@hotmail.com>
2025-02-11 12:10:58 -0300

    Creating random functions

--- src/index.js
+++ src/index.js
@@ -13,3 +13,59 @@ const player2 =  {
     poder: 4,
     pontos: 0,
 };
+
+async function rollDice() {
+    return Math.floor (Math.random() * 6) + 1;
+}
+
+async function getRandomBlock() {
+    let random = Math.random();
+    let result
+
+    switch (true) {
+        case random < 0.33:
+            result = "RETA";
+            break;
+        case random < 0.66:
+            result = "CURVA";
+            break
+        default:
+            result = "CONFRONTO";
+    }
+
+    return result
+}
+
+async function playRaceEngine(character1, character2) {
+    for (let round = 1; round <= 5; round++) {
+        console.log(`🏁 Rodada ${round}`);
+
+        //sortear bloco
+        let block = await getRandomBlock();
+        console.log(`Bloco: ${block}`);
+    }
+}
+
+async function getRandomBlock() {
+    let random = Math.random();
+    let result
+
+    switch (true) {
+        case random < 0.33:
+            result = "RETA";
+            break;
+        case random < 0.66:
+            result = "CURVA";
+            break
+        default:
+            result = "CONFRONTO";
+    }
+    
+    return result
+}
+
+async function main() {
+    console.log(`🏁🚨 Corrida entre ${player1.nome} e ${player2.nome} começando ...\n`);
+    await playRaceEngine(player1, player2);
+}
+main();
