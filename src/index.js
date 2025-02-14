3dca9397e6bef0824894a84335278ac5e2b6757c
Geovane Girard <g.ng2009@hotmail.com>
2025-02-14 11:21:22 -0300

    Including the draw of the confrontation element.

--- src/index.js
+++ src/index.js
@@ -14,6 +14,38 @@ const player2 =  {
     pontos: 0,
 };
 
+const player3 =  {
+    nome: "Bowser",
+    velocidade: 5,
+    manobrabilidade: 2,
+    poder: 5,
+    pontos: 0,
+};
+
+const player4 =  {
+    nome: "Peach",
+    velocidade: 3,
+    manobrabilidade: 4,
+    poder: 2,
+    pontos: 0,
+};
+
+const player5 =  {
+    nome: "Yoshi",
+    velocidade: 2,
+    manobrabilidade: 4,
+    poder: 3,
+    pontos: 0,
+};
+
+const player6 =  {
+    nome: "Donkey Kong",
+    velocidade: 2,
+    manobrabilidade: 2,
+    poder: 5,
+    pontos: 0,
+};
+
 async function rollDice() {
     return Math.floor (Math.random() * 6) + 1;
 }
@@ -35,6 +67,21 @@ async function getRandomBlock() {
 
     return result
 }
+
+async function getRandomElement() {
+    let fight = Math.random();
+    let effect
+
+    switch (true) {
+        case fight < 0.5:
+            effect = "CASCO";
+            break;
+        default:
+            effect = "BOMBA";
+    }
+
+    return effect
+}
 // const prompt = require('prompt-sync')();
 // const nome = prompt('Qual seu nome: ');
 // console.log("Olá " + nome);
@@ -51,6 +98,11 @@ async function playRaceEngine(character1, character2) {
         let block = await getRandomBlock();
         console.log(`Bloco: ${block}`);
 
+        let element = await getRandomElement();
+        if (block === "CONFRONTO") {
+            console.log(`Efeito: ${element}`)
+        }
+        
         //rolar dados
     let diceResult1 = await rollDice();
     let diceResult2 = await rollDice();
@@ -90,14 +142,20 @@ async function playRaceEngine(character1, character2) {
         await logRollResult(character1.nome, "poder", diceResult1, character1.poder);
         await logRollResult(character2.nome, "poder", diceResult2, character2.poder);
 
-        if (powerResult1 > powerResult2 && character2.pontos > 0) {
+        if (powerResult1 > powerResult2 && character2.pontos > 0 && element === "CASCO") {
             console.log(`${character1.nome} Venceu o confronto! ${character2.nome} perdeu um ponto 💥.`);
             character2.pontos --;
+        }else if (powerResult1 > powerResult2 && character2.pontos > 1 && element === "BOMBA") {
+            console.log(`${character1.nome} Venceu o confronto! ${character2.nome} perdeu dois pontos 💥.`);
+            character2.pontos -= 2;
         }
 
-        if (powerResult2 > powerResult1 && character1.pontos > 0) {
+        if (powerResult2 > powerResult1 && character1.pontos > 0 && element === "CASCO") {
             console.log(`${character2.nome} Venceu o confronto! ${character1.nome} perdeu um ponto 💥.`);
             character1.pontos --;
+        }else if (powerResult2 > powerResult1 && character1.pontos > 1 && element === "BOMBA") {
+            console.log(`${character2.nome} Venceu o confronto! ${character1.nome} perdeu dois pontos 💥.`);
+            character1.pontos -= 2;
         }
 
         console.log(powerResult2 === powerResult1 ? "Confronto empatado! Nenhum ponto foi perdido." : "");
@@ -113,24 +171,6 @@ async function playRaceEngine(character1, character2) {
     }
 
     console.log("---------------------------------------------")
-}
-
-async function getRandomBlock() {
-    let random = Math.random();
-    let result
-
-    switch (true) {
-        case random < 0.33:
-            result = "RETA";
-            break;
-        case random < 0.66:
-            result = "CURVA";
-            break
-        default:
-            result = "CONFRONTO";
-        }
-    
-        return result
     }
 }
 
